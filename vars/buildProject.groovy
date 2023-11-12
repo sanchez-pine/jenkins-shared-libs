da32221c8fad8fc349faa31110d6bb6b35d4a17d
Alexander Sosna <103523403+sanchez-pine@users.noreply.github.com>
2023-11-12 12:42:58 +0200

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -7,7 +7,7 @@ def call(Map config){
         git 'https://github.com/jglick/simple-maven-project-with-tests.git'
 
         // Run Maven on a Unix agent.
-        sh 'mvn -Dmaven.test.failure.ignore=true clean pakage'
+        sh 'mvn -Dmaven.test.failure.ignore=true clean package'
 
         // Grouping steps
         // sh '...'
